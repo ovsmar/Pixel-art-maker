e130a7676bd6dc75aded06d2793a3fc929e46651
ovsmar <ovsep.simonian@gmail.com>
2022-12-20 00:38:48 +0100

    save button works badly

--- app.js
+++ app.js
@@ -124,6 +124,7 @@ window.addEventListener("load", () => {
     }
 });
 
+//!save button works badly
 // Adds an event listener to a button element for clicks, allowing the user to save their drawing on a canvas as a PNG image file by downloading the image data.
 saveButton.addEventListener("click", () => {
     const image = document.createElement("img");
